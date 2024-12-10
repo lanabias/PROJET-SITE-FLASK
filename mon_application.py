37eafee5988eb8d4e43a1b119f55668786fd5077
lanabias <lnabias@etudelorenzofontes.fr>
2024-12-10 21:39:14 +0100

    Update mon_application.py

--- mon_application.py
+++ mon_application.py
@@ -2,3 +2,5 @@ from app.app import app
 
 if __name__ == "__main__":
     app.run(debug=app.config["DEBUG"])
+    app.run(PYTHONHASHSEED=app.config["PYTHONHASHSEED"]
+~                                                         
