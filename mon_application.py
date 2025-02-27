f6c2b3ceddaa66ea401be34f743f6df57278c98f
lanabias <lnabias@etudelorenzofontes.fr>
2025-02-27 12:12:31 +0100

    Delete mon_application.py

--- mon_application.py
+++
@@ -1,6 +0,0 @@
-from app.app import app
-
-if __name__ == "__main__":
-    app.run(debug=app.config["DEBUG"])
-    app.run(PYTHONHASHSEED=app.config["PYTHONHASHSEED"]
-~                                                         
